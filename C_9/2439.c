9c846dbd39a5156b9d87a252863147440f915dfc
minseo13 <128660782+minseo13@users.noreply.github.com>
2023-05-04 14:17:12 +0900

    Update 2439.c

--- C_9/2439.c
+++ C_9/2439.c
@@ -12,3 +12,26 @@ int main (void) {
     }
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main(void) 
+{
+    int i, j, k, n;
+    /*printf("입력하세요: ");
+    scanf("%d",&n);*/
+    for(i=1;i<=5;i++) 
+    {
+        for(k=1;k<=5-i;k++) 
+        {
+            printf(" ");
+        }
+        for(j=0;j<i;j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+    }
